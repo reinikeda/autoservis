6c49764dbe2f97e758f2c8e27ae99f5a44c91047
reinikeda <reinikeda@gmail.com>
2023-03-03 10:11:49 +0200

    -- delete unnecessary experimental file

--- autoservis/analytics/brand.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib as plt
-import seaborn as sns
-
-# %matplotlib inline
-
-brand_bar = sns.load_dataset('car_repair_servis_carmodel')
-print(brand_bar.head())
